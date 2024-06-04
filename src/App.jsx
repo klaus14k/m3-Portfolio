3aa1d6dbb33945d6a22b739aef2b801e80db0413
klaus14k <klauskuhlmann14@gmail.com>
2024-06-04 00:06:28 -0300

    fix: main settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import { Header } from "./components/Header/index.jsx";
-import { BannerSection } from "./components/BannerSection/index.jsx";
-import { AboutMeSection } from "./components/AboutMeSection/index.jsx";
-import { TechSection } from "./components/TechSection/index.jsx";
-import { ProjectSection } from "./components/ProjectSection/index.jsx";
-import { Footer } from "./components/Footer/index.jsx";
-import "./styles/reset.css"
-import "./styles/app.css";
+import { RoutesMain } from "./routes"
+import "./styles/reset.scss"
+import "./styles/app.css"
 
 function App() {
-
   return (
     <>
-      <Header />
-      <BannerSection />
-      <AboutMeSection />
-      <TechSection />
-      <ProjectSection />
-      <Footer />
+      <RoutesMain />
     </>
   )
 }
